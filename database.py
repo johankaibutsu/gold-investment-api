1ad07b3dfa52c10c8afe26017b167335b1b1db0d
johankaibutsu <kaibotsu_johan@outlook.com>
2025-08-25 18:11:58 +0530

    Polished

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ import datetime
 DATABASE_URL = "sqlite:///./chat_history.db"
 
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False} # Required for SQLite
+    DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
